aa67a4c7d22310bc194fcd5592e86a0496f1e668
“Wagner <wlancetti@gmail.com>
2024-09-02 01:50:10 -0300

    Atualização

---
+++ main.py
@@ -0,0 +1,5 @@
+from src import app  # Importa o app.py que está dentro do diretório src
+
+# A execução ficará no app.py
+if __name__ == "__main__":
+    app.run()  # Chama a função principal do app.py que controla a interface
